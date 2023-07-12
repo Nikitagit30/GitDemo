fbaba4b3a05f5047f6da6fcea2e84c0d2e9a0745
nikita <nikitajbhavsar@gmail.com>
2023-07-12 17:46:35 +0530

    first commit

---
+++ src/com/velocity/collections/GitExample.java
@@ -0,0 +1,12 @@
+package com.velocity.collections;
+
+public class GitExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("THIS IS MY FIRST GIT PROJECT");
+		
+
+	}
+
+}
